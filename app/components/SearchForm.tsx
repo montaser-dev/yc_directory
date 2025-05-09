c5f0473f158c81b618fbb41c089371a0efebcec4
montaser-dev <abdallahelmontaser786@gmail.com>
2025-05-09 19:18:28 +0100

    feat: implement SearchForm component with query handling and add SearchFormReset for form reset functionality

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
 
-const SearchForm = () => {
+const SearchForm = ({query}: {query?: string}) => {
+
   return (
-    <div>SearchForm</div>
+    <Form action="/" scroll={false} className='search-form'>
+      <input 
+      name='query'
+      defaultValue={query}
+      className='search-input'
+      placeholder='Search Startup'
+      />
+      
+      <div className='flex gap-2'>
+        {query && <SearchFormReset />}
+        <button type='submit' className='search-btn text-white'>
+          S
+        </button>
+      </div>
+      </Form>
   )
 }
 
